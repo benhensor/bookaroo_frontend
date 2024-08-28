7a8104e735ca6332aefc3e4b651e01b679d47958
benhensor <benhensor@gmail.com>
2024-08-28 18:09:17 +0100

    auth fix

--- src/context/MessagesContext.js
+++ src/context/MessagesContext.js
@@ -10,13 +10,13 @@ export const MessagesProvider = ({ children }) => {
 	const { user, isAuthenticated } = useAuth()
 
 	const fetchMessages = async () => {
-		console.log('fetchMessages:', user, isAuthenticated)
 		// Check if the user is authenticated before making the API call
 		// if (!isAuthenticated || !user) {
-		// 	throw new Error('User not authenticated')
-		// }
-		// console.log('messages context:', user)
-		if (user && isAuthenticated) {
+			// 	throw new Error('User not authenticated')
+			// }
+			// console.log('messages context:', user)
+			if (user && isAuthenticated) {
+			console.log('fetchMessages:', user, isAuthenticated)
 			const { data } = await axios.get(
 				`${process.env.REACT_APP_API_URL}/api/messages/inbox`,
 				{
