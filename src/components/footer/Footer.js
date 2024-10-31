b6b24f7246716412c954bc9f6a73b2b6d75f37f5
benhensor <benhensor@gmail.com>
2024-10-31 14:32:06 +0000

    additional instruction to login

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,35 +1,37 @@
 import React from 'react'
+import { useAuth } from '../../context/AuthContext'
 import { useDashboard } from '../../context/DashboardContext'
 import Browse from '../../icons/Browse'
 import Listings from '../../icons/Listings'
 import Messages from '../../icons/Messages'
 import Profile from '../../icons/Profile'
-import {
-	Container,
-	NavBar,
-	NavItem,
-} from '../../assets/styles/FooterStyles'
+import { Container, NavBar, NavItem } from '../../assets/styles/FooterStyles'
 
 export default function Footer() {
-	
+	const { user, isAuthenticated } = useAuth()
+
 	const { activePage, handlePageChange } = useDashboard()
 
 	return (
-		<Container>
-			<NavBar>
-				<NavItem onClick={() => handlePageChange('Browse')}>
-					<Browse isActive={activePage === 'Browse'} />
-				</NavItem>
-				<NavItem onClick={() => handlePageChange('Listings')}>
-					<Listings isActive={activePage === 'Listings'} />
-				</NavItem>
-				<NavItem onClick={() => handlePageChange('Messages')}>
-					<Messages isActive={activePage === 'Messages'} />
-				</NavItem>
-				<NavItem onClick={() => handlePageChange('Profile')}>
-					<Profile isActive={activePage === 'Profile'} />
-				</NavItem>
-			</NavBar>
-		</Container>
+		<>
+			{!user || !isAuthenticated ? null : (
+				<Container>
+					<NavBar>
+						<NavItem onClick={() => handlePageChange('Browse')}>
+							<Browse isActive={activePage === 'Browse'} />
+						</NavItem>
+						<NavItem onClick={() => handlePageChange('Listings')}>
+							<Listings isActive={activePage === 'Listings'} />
+						</NavItem>
+						<NavItem onClick={() => handlePageChange('Messages')}>
+							<Messages isActive={activePage === 'Messages'} />
+						</NavItem>
+						<NavItem onClick={() => handlePageChange('Profile')}>
+							<Profile isActive={activePage === 'Profile'} />
+						</NavItem>
+					</NavBar>
+				</Container>
+			)}
+		</>
 	)
 }
